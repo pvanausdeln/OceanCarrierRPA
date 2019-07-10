16c0574bff2ee9fed64bbcca7b1f3d2804edb64b
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-07-10 16:59:30 -0700

    switching main back for MSC

--- MSC/convertToPost.py
+++ MSC/convertToPost.py
@@ -159,5 +159,5 @@ def main(containerList, cwd):
             MSCPost(step)
 
 if __name__ == "__main__":
-    testMain(sys.argv[1])
-    #main(sys.argv[1], sys.argv[2])
+    #testMain(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
