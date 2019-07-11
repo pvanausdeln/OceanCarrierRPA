43675bf06b024ce334aaf5096cdeff7787be3087
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-07-11 14:56:31 -0700

    Yang Ming script done

--- YM/convertToPost.py
+++ YM/convertToPost.py
@@ -73,73 +73,79 @@ class baseInfo:
     "workOrderNumber": None
     }
 
-    StatusMapYM = {
-        "Gate out empty": "OA",
-        "Gate in empty": "I",
-        "Vessel departed": "VD",
-        "Vessel arrived": "VA",
-        "Loaded": "AE",
-        "Discharged": "UV"
-    }
-
-def YMCodeToName(code):
-    if(code == "AE"):
-        return "Loaded on Vessel"
-    elif(code == "VD"):
-        return "Vessel Departure"
-    elif(code == "VA"):
-        return "Vessel Arrival"
-    elif(code == "UV"):
-        return "Unloaded From Vessel"
-    elif(code == "OA"):
-        return "OUTGATE"
-    elif(code == "I"):
-        return "INGATE"
-    return None
 
 def YMEventTranslate(event):
-    for key, value in baseInfo.StatusMapYM.items():
-        if(event.find(key) != -1):
-            return value, YMCodeToName(value)
+    if(event.find("Empty to Shipper") != -1):
+        return ("Empty Equipment Dispatched", "EE")
+    elif(event.find("Received at Origin") != -1):
+        return ("Cargo Received", "CO")
+    elif(event.find("On Board") != -1):
+        return ("Loaded on Vessel", "AE")
+    elif(event.find("Discharged") != -1):
+        return ("Unloaded from Vessel", "UV")
+    elif(event.find("Load Outgate") != -1):
+        return ("Outgate Load", "OL")
+    elif(event.find("Empty Returned") != -1):
+        return ("Return Container", "RD")
+    elif(event.find("In Transit") != -1):
+        return ("In Transit", "I")
+    elif(event.find("Arrived") != -1):
+        return ("ARRIVED", "A")
+    elif(event.find("Loaded on Rail") != -1):
+        return ("LOADED_ON_RAIL", "AL")
+    elif(event.find("Notification") != -1):
+        return ("Notified", "N")
+    elif(event.find("Sending out empty container to other depot/terminal") != -1):
+        return ("Return Container", "RD")
     return (None, None)
     
+def YMPost(step):
+    with open(step) as jsonData:
+        data = json.load(jsonData)
+    postJson = copy.deepcopy(baseInfo.shipmentEventBase)
+    postJson["unitId"] = data.get("ContainerID")
+    postJson["location"] = data.get("At Facility")
+    postJson["eventTime"] = datetime.datetime.strptime(data.get("Date/Time"), '%Y/%m/%d %H:%M').strftime('%m-%d-%Y %H:%M') + ":00"
 
+    postJson["vessel"] = data.get("Vessel")
+    postJson["voyageNumber"] = data.get("Voyage")
+    #postJson["workOrderNumber"] = data.get("WONumber")
+    #postJson["billOfLadingNumber"] = data.get("BOLNumber")
 
-def YMPost(container, path):
-    if(os.path.isfile(path+"ContainerInformation\\"+container+".csv")):
-        with open(path+"ContainerInformation\\"+container+".csv") as containerInfo:
-            reader = csv.reader(containerInfo)
-            next(reader) # skip first row
-            for row in reader:
-                postJson = copy.deepcopy(baseInfo.shipmentEventBase)
-                postJson["unitId"] = container
-                postJson["location"] = row[1].split("\n")[0]
-                postJson["city"] = postJson["location"].split(",")[0]
-                if(row[2].strip() == "" or row[3].strip() == ""):
-                    continue
-                postJson["eventTime"] = datetime.datetime.strptime(row[2]+" "+row[3], '%Y-%m-%d %H:%M').strftime('%m-%d-%Y %H:%M:%S')
-                postJson["vessel"] = row[11]
-                postJson["voyageNumber"] = row[12]
-                postJson["workOrderNumber"] = row[14]
-                postJson["billOfLadingNumber"] = row[13]
-                postJson["eventCode"], postJson["eventName"] = YMEventTranslate(row[0])
-                postJson["resolvedEventSource"] = "YM RPA"
-                postJson["codeType"] = "UNLOCODE"
-                postJson["reportSource"] = "OceanEvent"
-                print(json.dumps(postJson))
-                if(postJson["eventCode"] == None):
-                    continue
-                headers = {'content-type':'application/json'}
-                r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
-                print(r)
+    postJson["eventName"], postJson["eventCode"] = YMEventTranslate(data.get("Event"))
+    postJson["resolvedEventSource"] = "YM RPA"
+    postJson["codeType"] = "UNLOCODE"
+    postJson["reportSource"] = "OceanEvent"
+    print(json.dumps(postJson))
+    if(postJson["eventCode"] == None):
+        return
+    headers = {'content-type':'application/json'}
+    r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
+    print(json.dumps(postJson))
     return
+
+def testMain(container): #test main
+    fileList = glob.glob(os.getcwd() + "\\ContainerInformation\\"+container+"Step*.json", recursive = True) #get all the json steps
+    if (not fileList):
+        return
+    fileList = [f for f in fileList if container in f] #set of steps for this number
+    fileList.sort(key=os.path.getmtime) #order steps correctly (by file edit time)
+    for step in fileList:
+        YMPost(step)
 
 def main(containerList, cwd):
     path=""
     for x in cwd.split("\\"):
         path+=x+"\\\\"
     for container in containerList:
-        YMPost(container, path)
+        fileList = glob.glob(r""+path+"ContainerInformation\\"+container+'Step*.json', recursive = True) #get all the json steps
+        if (not fileList):
+            continue
+        fileList = [f for f in fileList if container in f] #set of steps for this number
+        fileList.sort(key=os.path.getmtime) #order steps correctly (by file edit time)
+        for step in fileList:
+            YMPost(step)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    testMain(sys.argv[1])
+    #main(sys.argv[1], sys.argv[2])
