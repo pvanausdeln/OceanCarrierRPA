0fd2ad7f74b7310efcc66d27d0b2957f24582311
pvanausdeln <pvanausdeln@rez1.com>
2019-07-30 10:17:34 -0700

    containerCarrierMapping fixed

--- containerCarrierMapping.py
+++ containerCarrierMapping.py
@@ -21,25 +21,23 @@ excels = {}
 # for each carrier, filter all the rows with that carrier name
 # and download them as .xlsx
 for carrier in carriers:
-    # might not be needed in python 3 ("i coded this in python 2.7 quickly")
-    # converts from utf-8 to string
-    str_carrier = carrier.encode("utf-8")
 
     # returns the rows that contains a list of values in the carrier column
-    rows_df = df.loc[df['carrier'].isin( [str(str_carrier)])] #
+    rows_df = df.loc[df['carrier'].isin( [str(carrier)])] #
     rows_df.set_index('unitid', inplace=True)
-# finds the closest match to the keys in mapping.
-# i.e. APLU-BLU would match with the "APL" key in mapping.
-# play with the cutoff point range for accuracy tweaking. 0.0 - 1.0
-    carrier_prefix  = get_close_matches( str(str_carrier).upper(),carrier_prefixes,1,0.4)[0]
+
+    # finds the closest match to the keys in mapping.
+    # i.e. APLU-BLU would match with the "APL" key in mapping.
+    # play with the cutoff point range for accuracy tweaking. 0.0 - 1.0
+    carrier_prefix  = get_close_matches( str(carrier).upper(),carrier_prefixes,1,0.4)[0]
+
     # creates the prefix filename. CHANGE this for additional paths.
-    filename        = mapping[carrier_prefix] + "_Container_Tracking.xlsx" #Remove the ".\\..\\" if executed individually
+    filename = mapping[carrier_prefix] + "_Container_Tracking.xlsx" #Remove the ".\\..\\" if executed individually
     if( filename not in excels ):
         excels[filename] = rows_df
     else:
         frames = [ excels[filename] , rows_df ]
         excels[filename] = pd.concat(frames)
-    # print(carrier_prefix)
 
 
 for filename in excels:
