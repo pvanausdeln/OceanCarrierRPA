7158e2bc6c323b5472277f337454d5d485b080aa
sow-blu <49221631+sow-blu@users.noreply.github.com>
2019-07-30 17:05:28 +0530

    Data doesn't get into the individual excel files

--- containerCarrierMapping.py
+++ containerCarrierMapping.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 import os
 from difflib import get_close_matches
 
@@ -18,8 +18,8 @@ mapping = {
 
 carrier_prefixes = list( mapping.keys() )
 
-# df = dataframe. object class used by pandas to represent a matrix 
-df = pd.read_excel("Container_Tracking.xlsx")
+# df = dataframe. object class used by pandas to represent a matrix
+df = pd.read_excel(".\\..\\Container_Tracking.xlsx") #Change the path to "Container_Tracking.xlsx" if executed individually
 # grabs only unique carrier names
 carriers = df.carrier.unique()
 excels = {}
@@ -28,25 +28,24 @@ excels = {}
 # and download them as .xlsx
 for carrier in carriers:
     # might not be needed in python 3 ("i coded this in python 2.7 quickly")
-    # converts from utf-8 to string 
+    # converts from utf-8 to string
     str_carrier = carrier.encode("utf-8")
 
     # returns the rows that contains a list of values in the carrier column
-    rows_df = df.loc[ df['carrier'].isin( [str_carrier] ) ] #
+    rows_df = df.loc[df['carrier'].isin( [str(str_carrier)])] #
     rows_df.set_index('unitid', inplace=True)
 
-    carrier_prefix  = get_close_matches(str_carrier.upper(), carrier_prefixes, 1, 0.4)[0]
-    filename        = mapping[carrier_prefix] + "_Container_Tracking.xlsx"
+    carrier_prefix  = get_close_matches( str(str_carrier).upper(),carrier_prefixes,1,0.4)[0]
+    filename        = ".\\..\\" + mapping[carrier_prefix] + "_Container_Tracking.xlsx" #Remove the ".\\..\\" if executed individually
     if( filename not in excels ):
         excels[filename] = rows_df
     else:
         frames = [ excels[filename] , rows_df ]
         excels[filename] = pd.concat(frames)
+    # print(carrier_prefix)
 
 
 for filename in excels:
     if( os.path.exists( filename ) ):
         os.remove( filename )
     excels[filename].to_excel(filename)
-
-
