28aaae848bd9704635c7f203f34338d0af863a98
sow-blu <49221631+sow-blu@users.noreply.github.com>
2019-07-30 17:09:19 +0530

    Updated script to run from API call to executing UiPath process

---
+++ MultiProcess/ExecuteRobot.bat
@@ -0,0 +1,5 @@
+::Directory of the UiPathRobot.exe file
+cd C:\Users\sowrab.iyengar\AppData\Local\UiPath\app-19.7.0
+
+::Command line instruction to run the desired xaml file
+UiRobot.exe -file "C:\Users\sowrab.iyengar\Documents\UiPath\BlankProcess1\Main.xaml"
--- MultiProcess/pseudoCodeScript.py
+++ MultiProcess/pseudoCodeScript.py
@@ -1,9 +1,10 @@
 import requests
 import os
+import subprocess
 
 
-if(os.path.exists(r"C:\\Users\\pvanausdeln\\OneDrive - Blume Global\\UiPath\\OceanCarrierRPA\\Container_Tracking.xlsx")): # replace with correct filepath
-    os.remove(r"C:\\Users\\pvanausdeln\\OneDrive - Blume Global\\UiPath\\OceanCarrierRPA\\Container_Tracking.xlsx") # replace with correct filepath
+if(os.path.exists(r"D:\\blume-proj\\OceanCarrierRPA\\Container_Tracking.xlsx")): # replace with correct filepath
+    os.remove(r"D:\\blume-proj\\OceanCarrierRPA\\Container_Tracking.xlsx") # replace with correct filepath
 
 url = 'https://test-apps.blumesolutions.com/shipmentservice-api/v1/bv/vessel/track'
 filename = 'Container_Tracking.xlsx'
@@ -13,6 +14,11 @@ if r.status_code == 200:
     with open(filename, 'wb') as f:
         f.write(r.content)
 
-os.rename(r"C:\\Users\\pvanausdeln\\OneDrive - Blume Global\\Desktop\\Container_Tracking.xlsx", r"C:\\Users\\pvanausdeln\\OneDrive - Blume Global\\UiPath\\OceanCarrierRPA\\Container_Tracking.xlsx") # moves the API file, replace with correct filepath
+os.rename(r"D:\\blume-proj\\OceanCarrierRPA\\MultiProcess\\Container_Tracking.xlsx", r"D:\\blume-proj\\OceanCarrierRPA\\Container_Tracking.xlsx") # moves the API file, replace with correct filepath
+
+os.system("python .\\..\\containerCarrierMapping.py")
+
+#Add the path to UiRobot.exe as an env variable before executing the below command
+os.system("UiRobot -file D:\\blume-proj\\OceanCarrierRPA\\MultiProcess\\OceanCarrierRPAWithNames.xaml")
 
 #os.system("path to RPA batch file")
