a8e18096a2c2388d84b152e3c3ec3c10c89f83d5
sow-blu <49221631+sow-blu@users.noreply.github.com>
2019-08-01 15:39:43 +0530

    Created a schedule for it to run every hour

--- MultiProcess/ExecuteRobot.bat
+++ MultiProcess/ExecuteRobot.bat
@@ -1,3 +1,4 @@
+:Schedule
 ::The below runs the RPAProcess.py script
 
 python RPAProcess.py
@@ -13,3 +14,12 @@ cd C:\Users\sowrab.iyengar\AppData\Local\UiPath\app-19.7.0
 ::Run the UiRobot.exe with the file name as an argument, also change directory to your xaml file 
 
 UiRobot.exe -file C:\Users\sowrab.iyengar\Documents\OceanCarrierRPA\MultiProcess\OceanCarrierRPAWithNames.xaml
+
+::Change directory back to "Multiprocess" folder
+cd C:\Users\sowrab.iyengar\Documents\OceanCarrierRPA\MultiProcess
+
+::Run it after an hour again
+
+timeout /t 3600 /nobreak
+
+goto :Schedule
