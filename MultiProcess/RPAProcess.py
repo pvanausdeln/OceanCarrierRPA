ff02ca77d2504598bbdfe202c45aef04291d79ee
sow-blu <49221631+sow-blu@users.noreply.github.com>
2019-08-01 14:58:24 +0530

    Modified file so it doesn't call the other python script

--- MultiProcess/RPAProcess.py
+++ MultiProcess/RPAProcess.py
@@ -3,8 +3,8 @@ import os
 import subprocess
 
 
-if(os.path.exists(r"C:\\Users\\pvanausdeln\\OneDrive - Blume Global\\UiPath\\OceanCarrierRPA\\MultiProcess\\Container_Tracking.xlsx")): # replace with correct filepath
-    os.remove(r"C:\\Users\\pvanausdeln\\OneDrive - Blume Global\\UiPath\\OceanCarrierRPA\\MultiProcess\\Container_Tracking.xlsx") # replace with correct filepath
+if(os.path.exists(r"C:\\Users\\sowrab.iyengar\\Documents\\OceanCarrierRPA\\MultiProcess\\Container_Tracking.xlsx")): # replace with correct filepath
+    os.remove(r"C:\\Users\\sowrab.iyengar\\Documents\\OceanCarrierRPA\\MultiProcess\\Container_Tracking.xlsx") # replace with correct filepath
 
 url = 'https://test-apps.blumesolutions.com/shipmentservice-api/v1/bv/vessel/track'
 filename = 'Container_Tracking.xlsx'
@@ -14,7 +14,7 @@ if r.status_code == 200:
     with open(filename, 'wb') as f:
         f.write(r.content)
 
-os.system("python containerCarrierMapping.py")
-
-#Add the path to UiRobot.exe as an env variable before executing the below command
-os.system(r".\\ExecuteRobot.bat")
+# os.system("python containerCarrierMapping.py")
+#
+# #Add the path to UiRobot.exe as an env variable before executing the below command
+# os.system(r".\\ExecuteRobot.bat")
