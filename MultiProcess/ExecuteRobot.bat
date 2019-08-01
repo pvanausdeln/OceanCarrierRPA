1439e106ed3b7d2b44ab23a83c9e9a828127d712
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-08-01 14:43:15 -0700

    HAHA I GOT THE LAST ONE

--- MultiProcess/ExecuteRobot.bat
+++ MultiProcess/ExecuteRobot.bat
@@ -6,11 +6,13 @@ python RPAProcess.py
 ::Change directory to where UiRobot.exe is located
 pushd .
 
+set multi=%cd%
+
 cd %USERPROFILE%\AppData\Local\UiPath\app-19.7.0
 
 ::Run the UiRobot.exe with the file name as an argument, also change directory to your xaml file 
 
-UiRobot.exe -file "%USERPROFILE%\OneDrive - Blume Global\UiPath\OceanCarrierRPA\MultiProcess\OceanCarrierRPAWithNames.xaml"
+UiRobot.exe -file "%multi%\OceanCarrierRPAWithNames.xaml"
 
 ::Change directory back to "Multiprocess" folder
 popd
